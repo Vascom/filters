757b881887adc06ca83961b9505e60bdc14c2daf
Vasiliy Glazov <vascom2@gmail.com>
2011-08-22 14:46:14 +0400

    Quartus script: no output to stdout, more intellectual error handling

--- start_batch.sh
+++ start_batch.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
 
-quartus_map --64bit --parallel=on --read_settings_files=on --write_settings_files=off leon3mp -c leon3mp &&
-quartus_cdb --64bit --read_settings_files=on --write_settings_files=off leon3mp -c leon3mp --merge=on &&
-quartus_fit --64bit --parallel=8 --read_settings_files=on --write_settings_files=off leon3mp -c leon3mp &&
-quartus_sta --64bit --parallel=8 leon3mp -c leon3mp
-quartus_asm --64bit --read_settings_files=on --write_settings_files=off leon3mp -c leon3mp
+PROJECT_NAME="leon3mp"
+EN64="--64bit"
+READ_WRITE_SETTINGS="--read_settings_files=on --write_settings_files=off"
+
+echo "Start mapping"
+quartus_map $EN64 --parallel=on $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME &>longlog.map.log
+
+ERROR_PRESENT=`cat $PROJECT_NAME.map.rpt | grep -c Error`
+if [ $ERROR_PRESENT == 0 ]
+then
+    echo "Start merging"
+    quartus_cdb $EN64 $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME --merge=on 2>&1 1>>longlog.map.log
+else
+    echo "Finished with errors in mapping"
+    exit
+fi
+
+ERROR_PRESENT=`cat $PROJECT_NAME.merge.rpt | grep -c Error`
+if [ $ERROR_PRESENT == 0 ]
+then
+    echo "Start fitting"
+    quartus_fit $EN64 --parallel=4 $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME &>longlog.fit.log
+else
+    echo "Finished with errors in merging"
+    exit
+fi
+
+ERROR_PRESENT=`cat $PROJECT_NAME.fit.rpt | grep -c Error`
+if [ $ERROR_PRESENT == 0 ]
+then
+    echo "Start STA and assembling"
+    quartus_sta $EN64 --parallel=4 $PROJECT_NAME -c $PROJECT_NAME 1>/dev/null
+    quartus_asm $EN64 $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME 1>/dev/null &
+    cat $PROJECT_NAME.sta.rpt | head -n 2
+    cat $PROJECT_NAME.sta.rpt | head -n 159 | tail -n 9
+
+    echo "All finished"
+else
+    echo "Finished with errors in fitting"
+fi
+
