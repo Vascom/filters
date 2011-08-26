cfcaaee1dee8deba40101afcfb727004df61889b
Vasiliy Glazov <vascom2@gmail.com>
2011-08-26 11:04:18 +0400

    Implement functions for time processing

--- start_batch.sh
+++ start_batch.sh
@@ -3,26 +3,35 @@
 PROJECT_NAME="leon3mp"
 EN64="--64bit"
 READ_WRITE_SETTINGS="--read_settings_files=on --write_settings_files=off"
+
+function get_start_time() {
+    START_TIME=`date | cut -d " " -f4 | cut -d ":" -f1`
+    START_TIME_H=`expr $START_TIME \* 60`
+    START_TIME_M=`date | cut -d " " -f4 | cut -d ":" -f2`
+    START_TIME_FULL=`expr $START_TIME_H + $START_TIME_M`
+}
+
+function get_stop_time() {
+    STOP_TIME=`date | cut -d " " -f4 | cut -d ":" -f1`
+    STOP_TIME_H=`expr $STOP_TIME \* 60`
+    STOP_TIME_M=`date | cut -d " " -f4 | cut -d ":" -f2`
+    STOP_TIME_FULL=`expr $STOP_TIME_H + $STOP_TIME_M`
+}
+
+function print_time() {
+    PROCESSING_TIME=`expr $STOP_TIME_FULL - $START_TIME_FULL`
+    PROCESSING_TIME_H=`expr $PROCESSING_TIME / 60`
+    PROCESSING_TIME_M=`expr $PROCESSING_TIME % 60`
+    echo "processing time: $PROCESSING_TIME_H:$PROCESSING_TIME_M"
+}
 #======================================================================================================
 date
 echo -n "Start mapping "
-START_TIME=`date | cut -d " " -f4 | cut -d ":" -f1`
-START_TIME_H=`expr $START_TIME \* 60`
-START_TIME_M=`date | cut -d " " -f4 | cut -d ":" -f2`
-START_TIME_FULL=`expr $START_TIME_H + $START_TIME_M`
 
+get_start_time
 quartus_map $EN64 --parallel=on $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME &>longlog.map.log
-
-STOP_TIME=`date | cut -d " " -f4 | cut -d ":" -f1`
-STOP_TIME_H=`expr $STOP_TIME \* 60`
-STOP_TIME_M=`date | cut -d " " -f4 | cut -d ":" -f2`
-STOP_TIME_FULL=`expr $STOP_TIME_H + $STOP_TIME_M`
-
-PROCESSING_TIME=`expr $STOP_TIME_FULL - $START_TIME_FULL`
-PROCESSING_TIME_H=`expr $PROCESSING_TIME / 60`
-PROCESSING_TIME_M=`expr $PROCESSING_TIME % 60`
-
-echo "processing time: $PROCESSING_TIME_H:$PROCESSING_TIME_M"
+get_stop_time
+print_time
 
 ERROR_PRESENT=`cat $PROJECT_NAME.map.rpt | grep -c Error:`
 if [ $ERROR_PRESENT == 0 ]
@@ -37,24 +46,12 @@ fi
 ERROR_PRESENT=`cat $PROJECT_NAME.merge.rpt | grep -c Error:`
 if [ $ERROR_PRESENT == 0 ]
 then
-    echo "Start fitting"
-    START_TIME=`date | cut -d " " -f4 | cut -d ":" -f1`
-    START_TIME_H=`expr $START_TIME \* 60`
-    START_TIME_M=`date | cut -d " " -f4 | cut -d ":" -f2`
-    START_TIME_FULL=`expr $START_TIME_H + $START_TIME_M`
+    echo -n "Start fitting"
 
+    get_start_time
     quartus_fit $EN64 --parallel=4 $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME &>longlog.fit.log
-
-    STOP_TIME=`date | cut -d " " -f4 | cut -d ":" -f1`
-    STOP_TIME_H=`expr $STOP_TIME \* 60`
-    STOP_TIME_M=`date | cut -d " " -f4 | cut -d ":" -f2`
-    STOP_TIME_FULL=`expr $STOP_TIME_H + $STOP_TIME_M`
-
-    PROCESSING_TIME=`expr $STOP_TIME_FULL - $START_TIME_FULL`
-    PROCESSING_TIME_H=`expr $PROCESSING_TIME / 60`
-    PROCESSING_TIME_M=`expr $PROCESSING_TIME % 60`
-
-    echo "processing time: $PROCESSING_TIME_H:$PROCESSING_TIME_M"
+    get_stop_time
+    print_time
 else
     echo "Finished with errors in merging"
     exit
