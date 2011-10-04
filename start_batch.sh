2c18c69a730d50572b8be617b798daa524661559
Vascom <vascom2@gmail.com>
2011-10-04 17:57:01 +0400

    Corrected time processing

--- start_batch.sh
+++ start_batch.sh
@@ -5,33 +5,46 @@ EN64="--64bit"
 READ_WRITE_SETTINGS="--read_settings_files=on --write_settings_files=off"
 
 function get_start_time() {
-    START_TIME=`date | cut -d " " -f4 | cut -d ":" -f1`
+    if [ -e "$PROJECT_NAME.$1.rpt" ]
+    then
+        mv "$PROJECT_NAME.$1.rpt" "$PROJECT_NAME.$1.old.rpt"
+    fi
+    PTIME=0
+
+    START_TIME=`date | cut -d " " -f5 | cut -d ":" -f1`
     START_TIME_H=`expr $START_TIME \* 60`
-    START_TIME_M=`date | cut -d " " -f4 | cut -d ":" -f2`
+    START_TIME_M=`date | cut -d " " -f5 | cut -d ":" -f2`
     START_TIME_FULL=`expr $START_TIME_H + $START_TIME_M`
 }
 
 function get_stop_time() {
-    STOP_TIME=`date | cut -d " " -f4 | cut -d ":" -f1`
+    STOP_TIME=`date | cut -d " " -f5 | cut -d ":" -f1`
     STOP_TIME_H=`expr $STOP_TIME \* 60`
-    STOP_TIME_M=`date | cut -d " " -f4 | cut -d ":" -f2`
+    STOP_TIME_M=`date | cut -d " " -f5 | cut -d ":" -f2`
     STOP_TIME_FULL=`expr $STOP_TIME_H + $STOP_TIME_M`
-}
 
-function print_time() {
     PROCESSING_TIME=`expr $STOP_TIME_FULL - $START_TIME_FULL`
     PROCESSING_TIME_H=`expr $PROCESSING_TIME / 60`
     PROCESSING_TIME_M=`expr $PROCESSING_TIME % 60`
     echo "processing time: $PROCESSING_TIME_H:$PROCESSING_TIME_M"
 }
+
+function timer() {
+    while [ ! -e "$PROJECT_NAME.$1.rpt" ]
+    do
+        echo -ne "\r$PTIME min "
+        PTIME=`expr $PTIME + 1`
+        sleep 1m
+    done
+}
 #======================================================================================================
 date
-echo -n "Start mapping "
+echo "Start mapping "
 
-get_start_time
-quartus_map $EN64 --parallel=on $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME &>longlog.map.log
+get_start_time map
+quartus_map $EN64 --parallel=on $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME &>longlog.map.log &
+timer map
 get_stop_time
-print_time
 
 ERROR_PRESENT=`grep -c "Error:" "$PROJECT_NAME.map.rpt"`
 if [ $ERROR_PRESENT == 0 ]
@@ -46,12 +59,12 @@ fi
 ERROR_PRESENT=`grep -c "Error:" "$PROJECT_NAME.merge.rpt"`
 if [ $ERROR_PRESENT == 0 ]
 then
-    echo -n "Start fitting "
+    echo "Start fitting "
 
-    get_start_time
-    quartus_fit $EN64 --parallel=4 $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME &>longlog.fit.log
+    get_start_time fit
+    quartus_fit $EN64 --parallel=4 $READ_WRITE_SETTINGS $PROJECT_NAME -c $PROJECT_NAME &>longlog.fit.log &
+    timer fit
     get_stop_time
-    print_time
 else
     echo "Finished with errors in merging"
     exit
